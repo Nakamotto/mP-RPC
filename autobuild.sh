7597885edbe88e8d1215eeafe6dec96c53520d45
chaoxu <xuchao_202109@163.com>
2023-09-14 13:44:35 +0800

    update

---
+++ autobuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+if [ ! -d `pwd`/build ]; then
+    mkdir `pwd`/build
+fi
+
+rm -rf `pwd`/build/*
+
+cd `pwd`/build &&
+    cmake .. &&
+    make
+
+cd ..
+cp -r `pwd`/src/include `pwd`/lib
